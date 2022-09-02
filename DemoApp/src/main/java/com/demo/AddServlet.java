761ab09ad2ef6abb1117da2ebe01b1b0d77e1455
Abjeet Singh <abjeets50@gmail.com>
2022-09-02 14:18:52 +0530

    feat: request Dispatcher add

--- DemoApp/src/main/java/com/demo/AddServlet.java
+++ DemoApp/src/main/java/com/demo/AddServlet.java
@@ -2,20 +2,32 @@ package com.demo;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 public class AddServlet extends HttpServlet {
 	// Creating a Life Cycle
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i+j;
-		PrintWriter dedo = res.getWriter();
-		dedo.println(k);
+//		PrintWriter dedo = res.getWriter();
+//		dedo.println(k);
+		req.setAttribute("k",k);
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req,res);
+		
+		//Session Management - when you want to pass some data from one servlet to another
+	
+		
+	
 	}
 	
 	
-	//creating a post 
+	//Request Dispatcher
+	
+	
 }
